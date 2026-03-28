31240a89c0c019f9e1c8597c3ce2337b058b9cb3
Edbbioeco <edsonbbiologia@gmail.com>
2026-03-28 18:10:45 -0300

    Adicionado os dados de Saltinho

--- baixar_mapabiomas.R
+++ baixar_mapabiomas.R
@@ -9,3 +9,18 @@ library(datazoom.amazonia)
 library(terra)
 
 library(tidyterra)
+
+# Dados ----
+
+## Shapefile de Saltinho ----
+
+### Importando ----
+
+saltinho <- geobr::read_conservation_units() |>
+  sf::st_make_valid() |>
+  dplyr::filter(name_conservation_unit |> stringr::str_detect("SALTINHO"))
+
+### Visualizando ----
+
+ggplot() +
+  geom_sf(data = saltinho, color = "black")
