e3f8c59987dce7a6b75fb2b10df0c9d99b98c1d4
Edbbioeco <edsonbbiologia@gmail.com>
2026-04-04 11:15:21 -0300

    Sessão para baixar os dados de ocorrencia do GBIF

--- filtrar_distancia_por_cor_esp.R
+++ filtrar_distancia_por_cor_esp.R
@@ -17,3 +17,15 @@ library(fields)
 library(vegan)
 
 library(reshape2)
+
+# Dados ----
+
+## Registros de ocorrência ----
+
+### Baixar ----
+
+registros <- rgbif::occ_data(scientificName = "Boana albomarginata",
+                             hasCoordinate = TRUE,
+                             limit = 1e6,
+                             geometry = ma |> sf::st_bbox()) %>%
+  .$data
