eb669b8c16d739925a33bdd8c2c78fc4e362772d
Edbbioeco <edsonbbiologia@gmail.com>
2026-04-04 11:11:36 -0300

    Pacotes foram adicionados e carregados

---
+++ filtrar_distancia_por_cor_esp.R
@@ -0,0 +1,19 @@
+# Pacotes ----
+
+library(rgbif)
+
+library(tidyverse)
+
+library(geodata)
+
+library(terra)
+
+library(tidyterra)
+
+library(sf)
+
+library(fields)
+
+library(vegan)
+
+library(reshape2)
